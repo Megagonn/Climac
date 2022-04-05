6775b9cb700973f55a81fc16508acfeb7167520e
Meg <yusuffrasheed.yr@gmail.com>
2022-04-05 16:22:23 +0100

    first commit

---
+++ lib/model/weather.dart
@@ -0,0 +1,17 @@
+class Weather {
+  final String locationName, image, main, description;
+  final double wind, pressure, temp, humidity;
+
+  Weather({
+    required this.locationName,
+    required this.image,
+    required this.main,
+    required this.description,
+    required this.wind,
+    required this.pressure,
+    required this.temp,
+    required this.humidity,
+  });
+
+  
+}
