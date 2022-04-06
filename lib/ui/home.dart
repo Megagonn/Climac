d0ec3cb25fa5a489689069e12caa3ba09df47073
Meg <yusuffrasheed.yr@gmail.com>
2022-04-06 13:02:38 +0100

    commit

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -12,9 +12,9 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   var name = 'Yusuff';
 
-  // var result2;
-  // var result3;
-  // var result4;
+  var result2;
+  var result3;
+  var result4;
   // var result11;
   // var result21;
   // var result31;
@@ -23,12 +23,12 @@ class _HomeState extends State<Home> {
   getWeather() async {
     // var result1;
     var result1 = await Api().getData('london');
-    // result2 = await Api().getData('ede');
-    // result3 = await Api().getData('ogbomoso');
-    // result4 = await Api().getData('ohio');
-    // print(result1);
+    result2 = await Api().getData('ede');
+    result3 = await Api().getData('ogbomoso');
+    result4 = await Api().getData('ohio');
+    print(result1);
 
-    // List myList = [result1, result2, result3, result4];
+    List myList = [result1, result2, result3, result4];
     // setState(() {
     //   result11 = result1;
     //   result21 = result2;
@@ -36,7 +36,8 @@ class _HomeState extends State<Home> {
     //   result41 = result4;
     // });
     // print('---$result1');
-    return result1;
+    // return result1;
+    return myList;
   }
 
   @override
@@ -79,7 +80,7 @@ class _HomeState extends State<Home> {
                 height: 10,
               ),
               Container(
-                height: 300,
+                // height: 300,
                 child: SingleChildScrollView(
                   child: FutureBuilder(
                     future: getWeather(),
@@ -89,67 +90,81 @@ class _HomeState extends State<Home> {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return LinearProgressIndicator();
                       } else {
-                        var data = Weather.fromJson(snapshot.data);
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Card(
-                            color: const Color(0xFF6151C3),
-                            child: Container(
-                              padding: const EdgeInsets.all(20),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        data.country,
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white),
-                                      ),
-                                      Container(
-                                        margin: const EdgeInsets.only(
-                                          top: 10,
-                                          bottom: 30,
-                                        ),
-                                        child: Text(
-                                          data.locationName,
-                                          style: const TextStyle(
-                                              fontSize: 35,
-                                              fontWeight: FontWeight.w900,
-                                              color: Colors.white),
-                                        ),
-                                      ),
-                                      Text(
-                                        data.main,
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white),
-                                      ),
-                                    ],
+                        var allData = snapshot.data;
+                        // var data = Weather.fromJson(snapshot.data);
+                        return SingleChildScrollView(
+                          child: Container(
+                            height: MediaQuery.of(context).size.height-100,
+                            child: ListView.builder(
+                              itemCount: allData.length,
+                              itemBuilder: (context, val) {
+                                var data = Weather.fromJson(allData[val]);
+                                return Container(
+                                  margin:const EdgeInsets.symmetric(vertical: 10),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: const Color(0xFF6151C3),
+                                  // borderRadius: BorderRadius.circular(50),
                                   ),
-                                  Column(
-                                    children: [
-                                      Image(
-                                        image: NetworkImage(
-                                            "http://openweathermap.org/img/wn/${data.image}@2x.png"),
-                                      ),
-                                      Text(
-                                        "${data.temp.toString()}°C",
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
+                                  child: Container(
+                                    padding: const EdgeInsets.all(20),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              data.country,
+                                              style: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Colors.white),
+                                            ),
+                                            Container(
+                                              margin: const EdgeInsets.only(
+                                                top: 10,
+                                                bottom: 30,
+                                              ),
+                                              child: Text(
+                                                data.locationName,
+                                                style: const TextStyle(
+                                                    fontSize: 35,
+                                                    fontWeight: FontWeight.w900,
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                            Text(
+                                              data.main,
+                                              style: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        ),
+                                        Column(
+                                          children: [
+                                            Image(
+                                              image: NetworkImage(
+                                                  "http://openweathermap.org/img/wn/${data.image}@2x.png"),
+                                            ),
+                                            Text(
+                                              "${data.temp.toString()}°C",
+                                              style: const TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
                           ),
                         );
