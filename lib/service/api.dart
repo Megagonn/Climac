530ad7ff6b17f0c4fe797ece912005e63006fc99
Meg <yusuffrasheed.yr@gmail.com>
2022-05-07 18:22:20 +0100

    final commit

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:weather/service/token.dart';
 
 class Api {
   Future<dynamic> getData(String location) async {
+    var token = Token().token();
     var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=ca495b715da66e867fc2dd465dca9aac&units=metric');
+        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$token&units=metric');
     try {
       var response = await http.get(url);
       var res = await jsonDecode(response.body);
